9f9f3da7c1a07d5fec19a8d8365e76289fb856a2
janekjan <jas.piskorski@spoko.pl>
2018-11-27 09:35:08 +0100

    basic flask template

---
+++ main/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'main.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/henlo')
+    def henlo():
+        return 'Henlo , Warld!1'
+
+    return app
+
