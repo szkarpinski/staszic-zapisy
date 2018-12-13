ccb144c668c80f7f7a7d1a385a5159163effbbfb
janekjan <jas.piskorski@spoko.pl>
2018-12-13 15:22:52 +0100

    Minor bugfixes

--- main/zapisy.py
+++ main/zapisy.py
@@ -77,11 +77,11 @@ def nauczyciel(id):
             error = "Brakuje adresu e-mail."
         elif not rodo:
             error = "Brakuje zgody na przetwarzanie danych osobowych."
-        elif not godzina or not re.match('[0-2]?\d:[0:5]\d', godzina) or godzina != re.match('[0-2]\d:[0:5]\d', godzina)[0]:
+        elif not godzina or not re.match('[0-2]?\d:[0-5]\d', godzina) or godzina != re.match('[0-2]\d:[0-5]\d', godzina)[0]:
             error = "Godzina jest w nieodpowiednim formacie."
         #google reCAPTCHA
         elif not captcha.checkRecaptcha(captcha_response):
-            error = "Okropny z ciebie bot!!! "
+            error = "Okropny z ciebie bot!!!"
             
         # Trzeba zrobić jakoś transakcje
         elif not dane_nauczyciela['obecny']:
