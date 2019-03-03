e652a335f1bbd865d53e3c0f51860691b8a85c38
Szymon Karpiński <hugo@staszic.waw.pl>
2019-03-03 13:46:53 +0100

    bardzo bazowa baza panelu zarządzania

--- main/manage.py
+++ main/manage.py
@@ -1,4 +1,5 @@
 import configparser
+from main.db import get_db
 from flask import (
     Blueprint, render_template, flash
 )
@@ -9,6 +10,10 @@ bp = Blueprint('manage', __name__)
 @bp.route('/manage/<string:key>', methods=('GET', 'POST'))
 def panel(key):
     db = get_db()
+    email = 'hugo@staszic.waw.pl'
 
-    return rander_template('zapisy/manage.html')
+    terminy = db.execute(
+        'SELECT * FROM wizyty JOIN nauczyciele ON wizyty.id_nauczyciela=nauczyciele.id WHERE email_rodzica = ? ORDER BY godzina', (email,)
+    ).fetchall()
+    return render_template('zapisy/manage.html', email=email, terminy=terminy)
 
