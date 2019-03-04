021a1a7ffcf4036b075848a65aa503f143483672
Alex <alsabak@gmail.com>
2019-03-04 14:39:13 +0100

    itsdangerous działa na pseudokontach

--- main/manage.py
+++ main/manage.py
@@ -1,19 +1,70 @@
 import configparser
+import functools
+from itsdangerous import Serializer, BadSignature
 from main.db import get_db
 from flask import (
-    Blueprint, render_template, flash
+    Blueprint, render_template, flash, redirect, url_for, session, g
 )
 from main.db import get_db
 
-bp = Blueprint('manage', __name__)
 
-@bp.route('/manage/<string:key>', methods=('GET', 'POST'))
-def panel(key):
+bp = Blueprint('manage', __name__, url_prefix='/manage')
+
+
+@bp.before_app_request
+def load_parent():
     db = get_db()
-    email = 'janekjan@staszic.waw.pl'
+    id = session['rodzic']
 
+    if id is None:
+        g.rodzic = None
+    else:
+        g.rodzic = db.execute(
+            'SELECT * FROM rodzice WHERE id = ?',
+            (id,)
+        ).fetchone()
+
+def auth_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.rodzic is None:
+            return redirect(url_for('index'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+@bp.route('/auth/<string:key>')
+def auth(key):
+    db = get_db()
+    valid, id = Serializer('XD').loads_unsafe(key)
+    if not valid:
+        flash('Niepoprawny hash. Sprawdź czy nie pomyliłeś się przy przepisywaniu.')
+        return redirect(url_for('index'))
+    rodzic = db.execute(
+        'SELECT * FROM rodzice WHERE id = ?',
+        (id,)
+    ).fetchone()        
+    if not rodzic:
+        flash('Niepoprawny numer nauczyciela.')
+        return redirect(url_for('index'))
+    else:
+        session.clear()
+        session['rodzic'] = id
+        return redirect(url_for('manage.panel'))
+
+@bp.route('/', methods=('GET', 'POST'))
+@auth_required
+def panel():
+    db = get_db()
+    id = session['rodzic']
+    rodzic = db.execute(
+        'SELECT * FROM rodzice WHERE id = ?',
+        (id,)
+    ).fetchone()
     terminy = db.execute(
-        'SELECT * FROM wizyty JOIN nauczyciele ON wizyty.id_nauczyciela=nauczyciele.id WHERE email_rodzica = ? ORDER BY godzina', (email,)
+        'SELECT * FROM wizyty JOIN nauczyciele ON nauczyciele.id = wizyty.id_nauczyciela '
+        'WHERE id_rodzica = ? ORDER BY godzina', (rodzic['id'],)
     ).fetchall()
-    return render_template('zapisy/manage.html', email=email, terminy=terminy)
+    return render_template('zapisy/manage.html', email=rodzic['email'], terminy=terminy)
 
